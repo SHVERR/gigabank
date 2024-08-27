59ff19002ce5db3bfeba27df05723d343b0348e6
SHVERR <shevlad82@gmail.com>
2024-08-27 18:37:27 +0300

    Update UserController.java

--- src/main/java/gigabank/controllers/UserController.java
+++ src/main/java/gigabank/controllers/UserController.java
@@ -1,33 +1,18 @@
 package gigabank.controllers;
 
-homework-1
 import gigabank.entity.User;
 import gigabank.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import gigabank.entity.User;
-import gigabank.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-master
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
-homework-1
-
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
@@ -40,8 +25,7 @@ homework-1
 
     @PostMapping
     public User createUser(@RequestBody User user) {
-        userService.addUser(user);
-        return user;
+        return userService.addUser(user);
     }
 
     @PatchMapping("/{id}")
@@ -54,38 +38,3 @@ homework-1
         userService.deleteUser(id);
     }
 }
-
- private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping
-    public List<User> getUsers() {
-        return userService.getUsers();
-    }
-
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable("id") String id) {
-        return userService.getUserById(id);
-    }
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        userService.addUser(user);
-        return user;
-    }
-
-    @PatchMapping("/{id}")
-    public User updateUser(@PathVariable("id") String id, @RequestBody User user) {
-        return userService.updateUser(id, user);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable("id") String id) {
-        userService.deleteUser(id);
-    }
-}
-master
