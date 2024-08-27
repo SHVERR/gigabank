d6f04551cc68d1c7b260c2bb0071d4f44fa9512c
SHVERR <shevlad82@gmail.com>
2024-08-27 18:39:37 +0300

    Update BankAccountController.java

--- src/main/java/gigabank/controllers/BankAccountController.java
+++ src/main/java/gigabank/controllers/BankAccountController.java
@@ -1,23 +1,17 @@
 package gigabank.controllers;
 
-homework-1
 import gigabank.entity.BankAccount;
 import gigabank.service.BankAccountService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/bank_accounts")
+@RequiredArgsConstructor
 public class BankAccountController {
-
     private final BankAccountService bankAccountService;
-
-    @Autowired
-    public BankAccountController(BankAccountService bankAccountService) {
-        this.bankAccountService = bankAccountService;
-    }
 
     @GetMapping
     public List<BankAccount> getBankAccounts() {
@@ -31,8 +25,7 @@ public class BankAccountController {
 
     @PostMapping
     public BankAccount createBankAccount(@RequestBody BankAccount bankAccount) {
-        bankAccountService.addBankAccount(bankAccount);
-        return bankAccount;
+        return bankAccountService.addBankAccount(bankAccount);
     }
 
     @PatchMapping("/{id}")
@@ -44,51 +37,4 @@ public class BankAccountController {
     public void deleteBankAccount(@PathVariable("id") String id) {
         bankAccountService.deleteBankAccount(id);
     }
-
-import org.springframework.web.bind.annotation.*;
-
-import gigabank.entity.BankAccount;
-import gigabank.service.BankAccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/bank_accounts")
-public class BankAccountController {
-
-    private final BankAccountService bankAccountService;
-
-    @Autowired
-    public BankAccountController(BankAccountService bankAccountService) {
-        this.bankAccountService = bankAccountService;
-    }
-
-    @GetMapping
-    public List<BankAccount> getBankAccounts() {
-        return bankAccountService.getBankAccounts();
-    }
-
-    @GetMapping("/{id}")
-    public BankAccount getBankAccount(@PathVariable("id") String id) {
-        return bankAccountService.getBankAccountById(id);
-    }
-
-    @PostMapping
-    public BankAccount createBankAccount(@RequestBody BankAccount bankAccount) {
-        bankAccountService.addBankAccount(bankAccount);
-        return bankAccount;
-    }
-
-    @PatchMapping("/{id}")
-    public BankAccount updateBankAccount(@PathVariable("id") String id, @RequestBody BankAccount bankAccount) {
-        return bankAccountService.updateBankAccount(id, bankAccount);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteBankAccount(@PathVariable("id") String id) {
-        bankAccountService.deleteBankAccount(id);
-    }
-master
 }
