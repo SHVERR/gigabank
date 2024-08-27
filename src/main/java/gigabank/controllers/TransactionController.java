e63dc1b238cc574d705576ccb32a145f159bbf4e
SHVERR <shevlad82@gmail.com>
2024-08-27 18:40:28 +0300

    Update TransactionController.java

--- src/main/java/gigabank/controllers/TransactionController.java
+++ src/main/java/gigabank/controllers/TransactionController.java
@@ -1,32 +1,17 @@
 package gigabank.controllers;
 
-homework-1
 import gigabank.entity.Transaction;
 import gigabank.service.TransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import gigabank.entity.Transaction;
-import gigabank.service.TransactionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-master
 
 @RestController
 @RequestMapping("/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
-homework-1
-
     private final TransactionService transactionService;
-
-    @Autowired
-    public TransactionController(TransactionService transactionService) {
-        this.transactionService = transactionService;
-    }
 
     @GetMapping
     public List<Transaction> getTransactions() {
@@ -40,8 +25,7 @@ homework-1
 
     @PostMapping
     public Transaction createTransaction(@RequestBody Transaction transaction) {
-        transactionService.addTransaction(transaction);
-        return transaction;
+        return transactionService.addTransaction(transaction);
     }
 
     @PatchMapping("/{id}")
@@ -54,38 +38,3 @@ homework-1
         transactionService.deleteTransaction(id);
     }
 }
-
-    private final TransactionService transactionService;
-
-    @Autowired
-    public TransactionController(TransactionService transactionService) {
-        this.transactionService = transactionService;
-    }
-
-    @GetMapping
-    public List<Transaction> getTransactions() {
-        return transactionService.getTransactions();
-    }
-
-    @GetMapping("/{id}")
-    public Transaction getTransaction(@PathVariable("id") String id) {
-        return transactionService.getTransactionById(id);
-    }
-
-    @PostMapping
-    public Transaction createTransaction(@RequestBody Transaction transaction) {
-        transactionService.addTransaction(transaction);
-        return transaction;
-    }
-
-    @PatchMapping("/{id}")
-    public Transaction updateTransaction(@PathVariable("id") String id, @RequestBody Transaction transaction) {
-        return transactionService.updateTransaction(id, transaction);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteTransaction(@PathVariable("id") String id) {
-        transactionService.deleteTransaction(id);
-    }
-}
-master
